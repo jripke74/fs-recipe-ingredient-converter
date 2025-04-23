569ee537cfb06cb7297c207e17d05e42b71da90d
Jeff Ripke <jripke74@gmail.com>
2025-04-23 07:40:33 -0700

    add code

--- script.js
+++ script.js
@@ -45,4 +45,34 @@ const conversionTable = {
 // Start by declaring a convertQuantity function. It should have
 // a single fromUnit parameter, and should be empty.
 
-const convertQuantity = (fromUnit) => {};
+// Step 7
+// Example Code
+// const tikkaMasala = (spice) => (protein) => (vegetable) => {
+//   return "Yum!";
+// }
+// Now, following the syntax above, update your convertQuantity
+// function to return a second function. This inner function
+// should accept a toUnit argument, and should be empty.
+
+// Step 8
+// Example Code
+// const tikkaMasala = (spice) => (protein) => (vegetable) => {
+//   return "Yum!";
+// }
+// Finally, update your inner function to return a third function.
+// This third function should also accept a single argument named
+// quantity, and have an empty body.
+
+// Step 9
+// Your innermost curried function will hold the logic for converting
+// the value.
+
+// First, use the fromUnit argument to access your conversionTable object,
+// then use the toUnit argument to access the nested object.
+
+// Return the result of multiplying that conversion value with the quantity
+// value.
+
+const convertQuantity = (fromUnit) => (toUnit) => (quantity) => {
+  return convertQuantity("gram")("cup")(1);
+};
